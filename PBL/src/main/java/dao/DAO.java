6fd8cf7621b205b42ab4a3ace47fd7103e25e23f
Márcio Roberto <marcio.roberto0739@gmail.com>
2023-03-20 14:49:10 -0300

    feat: TecnicoDAO adicionado no DAO

--- PBL/src/main/java/dao/DAO.java
+++ PBL/src/main/java/dao/DAO.java
@@ -2,14 +2,25 @@ package dao;
 
 import dao.cliente.ClienteDAO;
 import dao.cliente.ClienteLista;
+import dao.tecnico.TecnicoDAO;
+import dao.tecnico.TecnicoLista;
 
 public class DAO {
     private static ClienteDAO clienteDAO;
+
+    private static TecnicoDAO tecnicoDAO;
 
     public static ClienteDAO getCliente(){
         if (clienteDAO == null){
             clienteDAO = new ClienteLista();
         }
         return clienteDAO;
+    }
+
+    public static TecnicoDAO getTecnico(){
+        if (tecnicoDAO == null){
+            tecnicoDAO = new TecnicoLista();
+        }
+        return tecnicoDAO;
     }
 }
