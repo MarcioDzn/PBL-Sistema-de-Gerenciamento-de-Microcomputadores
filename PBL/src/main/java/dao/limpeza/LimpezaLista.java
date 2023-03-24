aea2a29b4dcda5aefbe9faa0479692414a6c9124
Márcio Roberto <marcio.roberto0739@gmail.com>
2023-03-24 18:58:56 -0300

    feat: Classe LimpezaLista

---
+++ PBL/src/main/java/dao/limpeza/LimpezaLista.java
@@ -0,0 +1,67 @@
+package dao.limpeza;
+
+import model.Instalacao;
+import model.Limpeza;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LimpezaLista implements LimpezaDAO {
+    private List<Limpeza> listaLimpeza;
+    private int proxId;
+
+    public LimpezaLista() {
+        this.listaLimpeza = new LinkedList<Limpeza>();
+        this.proxId = 0;
+    }
+
+    @Override
+    public void adicionarNaLista(Limpeza objeto) {
+        objeto.setId(this.proxId);
+        listaLimpeza.add(objeto);
+
+        this.proxId++;
+    }
+
+    @Override
+    public Limpeza buscarPorId(int id) {
+        for (Limpeza limpeza : listaLimpeza){
+            if (limpeza.getId() == id)
+                return limpeza;
+        }
+        return null;
+    }
+
+    @Override
+    public List<Limpeza> buscarTodos() {
+        List<Limpeza> lista = new LinkedList<Limpeza>();
+
+        for (Limpeza limpeza : listaLimpeza){
+            lista.add(limpeza);
+        }
+
+        return lista;
+    }
+
+    @Override
+    public void atualizar(Limpeza objeto) {
+        for (int i = 0; i < this.listaLimpeza.size(); i++){
+            if (listaLimpeza.get(i).getId() == objeto.getId()){
+                this.listaLimpeza.add(objeto.getId(), objeto);
+
+                return; // Para o laço assim que entrar no if e atualizar a lista
+            }
+        }
+    }
+
+    @Override
+    public void remover(Limpeza objeto) {
+        for (int i = 0; i < this.listaLimpeza.size(); i++){
+            if (listaLimpeza.get(i).getId() == objeto.getId()){
+                this.listaLimpeza.remove(objeto);
+
+                return;
+            }
+        }
+    }
+}
