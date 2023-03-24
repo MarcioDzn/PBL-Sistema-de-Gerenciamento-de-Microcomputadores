b0dc73252b727a89895af9d094cead9f82aed8b1
Márcio Roberto <marcio.roberto0739@gmail.com>
2023-03-24 18:19:22 -0300

    feat: Classe OutroComponenteLista

---
+++ PBL/src/main/java/dao/outroComponente/OutroComponenteLista.java
@@ -0,0 +1,68 @@
+package dao.outroComponente;
+
+import model.Cliente;
+import model.OrdemServico;
+import model.OutroComponente;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class OutroComponenteLista implements OutroComponenteDAO{
+    private List<OutroComponente> listaOutrosComponentes;
+    private int proxId;
+
+    public OutroComponenteLista() {
+        this.listaOutrosComponentes = new LinkedList<OutroComponente>();
+        this.proxId = 0;
+    }
+
+    @Override
+    public void adicionarNaLista(OutroComponente objeto) {
+        objeto.setId(this.proxId);
+        listaOutrosComponentes.add(objeto);
+
+        this.proxId++;
+    }
+
+    @Override
+    public OutroComponente buscarPorId(int id) {
+        for (OutroComponente outroComponente : listaOutrosComponentes){
+            if (outroComponente.getId() == id)
+                return outroComponente;
+        }
+        return null;
+    }
+
+    @Override
+    public List<OutroComponente> buscarTodos() {
+        List<OutroComponente> lista = new LinkedList<OutroComponente>();
+
+        for (OutroComponente outroComponente : listaOutrosComponentes){
+            lista.add(outroComponente);
+        }
+
+        return lista;
+    }
+
+    @Override
+    public void atualizar(OutroComponente objeto) {
+        for (int i = 0; i < this.listaOutrosComponentes.size(); i++){
+            if (listaOutrosComponentes.get(i).getId() == objeto.getId()){
+                this.listaOutrosComponentes.add(objeto.getId(), objeto);
+
+                return; 
+            }
+        }
+    }
+
+    @Override
+    public void remover(OutroComponente objeto) {
+        for (int i = 0; i < this.listaOutrosComponentes.size(); i++){
+            if (listaOutrosComponentes.get(i).getId() == objeto.getId()){
+                this.listaOutrosComponentes.remove(objeto);
+
+                return;
+            }
+        }
+    }
+}
