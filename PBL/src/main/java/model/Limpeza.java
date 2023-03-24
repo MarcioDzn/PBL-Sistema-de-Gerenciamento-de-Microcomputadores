baba2c8b3daf64f5877f20bd88bf0d2182a2b1e5
Márcio Roberto <marcio.roberto0739@gmail.com>
2023-03-24 18:58:23 -0300

    feat(Limpeza): Atributo id

--- PBL/src/main/java/model/Limpeza.java
+++ PBL/src/main/java/model/Limpeza.java
@@ -1,7 +1,16 @@
 package model;
 
 public class Limpeza extends Servico{
+    private int id;
     public Limpeza(double preco, double custo) {
         super(preco, custo);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
