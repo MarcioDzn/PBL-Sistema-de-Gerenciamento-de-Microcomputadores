9ed8315b281f6590bd99fa8ed59064c6ec0f4bb4
Márcio Roberto <marcio.roberto0739@gmail.com>
2023-03-26 17:33:21 -0300

    feat: Atualização da classe DAO

--- PBL/src/main/java/dao/DAO.java
+++ PBL/src/main/java/dao/DAO.java
@@ -2,10 +2,21 @@ package dao;
 
 import dao.cliente.ClienteDAO;
 import dao.cliente.ClienteLista;
+import dao.instalacao.InstalacaoDAO;
+import dao.instalacao.InstalacaoLista;
+import dao.limpeza.LimpezaDAO;
+import dao.limpeza.LimpezaLista;
+import dao.montagem.MontagemDAO;
+import dao.montagem.MontagemLista;
 import dao.ordemServico.OrdemServicoDAO;
 import dao.ordemServico.OrdemServicoLista;
+import dao.outroComponente.OutroComponenteDAO;
+import dao.outroComponente.OutroComponenteLista;
+import dao.peca.PecaDAO;
+import dao.peca.PecaLista;
 import dao.tecnico.TecnicoDAO;
 import dao.tecnico.TecnicoLista;
+
 
 public class DAO {
     private static ClienteDAO clienteDAO;
@@ -14,11 +25,45 @@ public class DAO {
 
     private static OrdemServicoDAO ordemServicoDAO;
 
+    private static InstalacaoDAO instalacaoDAO;
+
+    private static LimpezaDAO limpezaDAO;
+
+    private static MontagemDAO montagemDAO;
+
+    private static OutroComponenteDAO outroComponenteDAO;
+
+    private static PecaDAO pecaDAO;
+
     public static ClienteDAO getCliente(){
         if (clienteDAO == null){
             clienteDAO = new ClienteLista();
         }
         return clienteDAO;
+    }
+
+    public static InstalacaoDAO getInstalacao(){
+        if (instalacaoDAO == null){
+            instalacaoDAO = new InstalacaoLista();
+
+        }
+        return instalacaoDAO;
+    }
+
+    public static LimpezaDAO getLimpeza(){
+        if (limpezaDAO == null){
+            limpezaDAO = new LimpezaLista();
+
+        }
+        return limpezaDAO;
+    }
+
+    public static MontagemDAO getMontagem(){
+        if (montagemDAO == null){
+            montagemDAO = new MontagemLista();
+
+        }
+        return montagemDAO;
     }
 
     public static TecnicoDAO getTecnico(){
@@ -34,4 +79,20 @@ public class DAO {
         }
         return ordemServicoDAO;
     }
+
+    public static OutroComponenteDAO getOutroComponente(){
+        if (outroComponenteDAO == null){
+            outroComponenteDAO = new OutroComponenteLista();
+        }
+        return outroComponenteDAO;
+    }
+
+    public static PecaDAO getPeca(){
+        if (pecaDAO == null){
+            pecaDAO = new PecaLista();
+        }
+        return pecaDAO;
+    }
+
+
 }
