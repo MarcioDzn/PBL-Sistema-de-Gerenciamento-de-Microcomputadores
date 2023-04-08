6293fac9719cf93a4af78f6b289cd664414c9a3d
Márcio Roberto <marcio.roberto0739@gmail.com>
2023-04-08 12:52:31 -0300

    :test_tube: test: Classe LimpezaTest

---
+++ PBL/src/test/java/com/example/pbl/model/LimpezaTest.java
@@ -0,0 +1,60 @@
+package com.example.pbl.model;
+
+import com.example.pbl.dao.DAO;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LimpezaTest {
+    private Limpeza limpeza;
+    @BeforeEach
+    void setUp(){
+        this.limpeza = new Limpeza(10, 10);
+        DAO.getLimpeza().criar(this.limpeza);
+    }
+
+    @Test
+    void getOrdensServico() {
+        List<OrdemServico> lista = new LinkedList<OrdemServico>();
+
+//        for (int i = 0; i < 2; i++){
+//            lista.add(new OrdemServico(i));
+//            lista.get(i).addServicos(this.limpeza);
+//            DAO.getOrdemServico().criar(lista.get(i));
+//        }
+
+        OrdemServico os1 = new OrdemServico(0);
+        os1.addServicos(this.limpeza);
+        DAO.getOrdemServico().criar(os1);
+
+        lista.add(os1);
+
+        assertEquals(lista, DAO.getOrdemServico().buscarPorServico(0, "Limpeza"));
+
+        lista.add(new OrdemServico(4));
+
+        assertNotEquals(lista, DAO.getOrdemServico().buscarPorServico(0, "Limpeza"));
+    }
+
+    @Test
+    void testEquals() {
+        Limpeza limpeza2 = new Limpeza(10, 15);
+        limpeza2.setId(0);
+
+        assertTrue(this.limpeza.equals(limpeza2));
+
+        limpeza2.setId(1);
+
+        assertFalse(this.limpeza.equals(limpeza2));
+    }
+
+    @AfterEach
+    void tearDown(){
+        DAO.getLimpeza().deletarTudo();
+    }
+}
