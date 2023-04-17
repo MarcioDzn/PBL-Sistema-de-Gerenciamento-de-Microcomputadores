ca1247127c8c9f1b4c197d84716e2a42f11986a9
Márcio Roberto <marcio.roberto0739@gmail.com>
2023-04-17 15:32:41 -0300

    :wrench: chore: junit e awt adicionados em module-info

--- PBL/src/main/java/module-info.java
+++ PBL/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.pbl {
+    requires org.junit.jupiter.api;
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.pbl to javafx.fxml;
