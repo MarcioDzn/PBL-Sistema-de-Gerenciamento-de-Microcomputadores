1871bda7f055540e5940290ba7cb241b873439be
Márcio Roberto <marcio.roberto0739@gmail.com>
2023-06-13 18:26:09 -0300

    :sparkles: feat(OrdensCard): Adicionado novo card

--- PBL/src/main/java/com/example/pbl/utils/componentesJavafx/OrdensCard.java
+++ PBL/src/main/java/com/example/pbl/utils/componentesJavafx/OrdensCard.java
@@ -65,6 +65,65 @@ public class OrdensCard extends ComponenteJavaFx {
         return hbox;
     }
 
+    private static HBox criarComponenteNovoOrdemAtual(Map<String, String> dados, int width, int height){
+        int FONT_SIZE = 15;
+
+        VBox vbox = new VBox();
+        vbox.setPadding(new Insets(5, 5, 5, 5));
+
+        VBox vboxGeral = new VBox();
+        HBox hbox = criarComponente(width, height);
+
+        Label objLabel;
+        Label objLabelInfo;
+        for (String nome : dados.keySet()){
+            if (!nome.equals("Id")) {
+                objLabel = new Label(nome);
+                objLabelInfo = new Label(dados.get(nome));
+
+                objLabel.setFont(new javafx.scene.text.Font("Arial", FONT_SIZE * 1.25));
+                objLabel.setTextFill(Color.WHITE);
+
+                objLabelInfo.setFont(new javafx.scene.text.Font("Arial", FONT_SIZE));
+                objLabelInfo.setTextFill(Color.WHITE);
+
+                vbox.getChildren().add(objLabel);
+                vbox.getChildren().add(objLabelInfo);
+
+                vboxGeral.getChildren().add(vbox);
+
+                vbox = new VBox();
+                vbox.setPadding(new Insets(5, 5, 5, 5));
+            }
+        }
+
+
+        Button buttonFinalizar = new Button("Finalizar");
+        buttonFinalizar.setMinWidth(width);
+        buttonFinalizar.setId(dados.get("Nome") + "-" + dados.get("Id"));
+        vbox.getChildren().add(buttonFinalizar);
+
+        Button buttonCancelar = new Button("Cancelar");
+
+        buttonCancelar.setMinWidth(width);
+        buttonCancelar.setId(dados.get("Nome") + "-" + dados.get("Id"));
+        vbox.getChildren().add(buttonCancelar);
+
+        Button buttonEditar = new Button("Editar");
+        buttonEditar.setMinWidth(width);
+        buttonEditar.setId(dados.get("Nome") + "-" + dados.get("Id"));
+        vbox.getChildren().add(buttonEditar);
+
+        vbox.setSpacing(10);
+
+        vboxGeral.getChildren().add(vbox);
+
+        hbox.getChildren().add(vboxGeral);
+        hbox.setId(String.valueOf(dados.get("Id")));
+
+        return hbox;
+    }
+
     public static FlowPane criarTabela(List<OrdemServico> listaItens, int width, int height) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Map<String, String> dados;
         FlowPane flowPane = new FlowPane();
@@ -74,6 +133,30 @@ public class OrdensCard extends ComponenteJavaFx {
             dados = getDados(item);
 
             hbox = criarComponenteNovo(dados, width, height);
+
+            flowPane.getChildren().add(hbox);
+        }
+
+        // Configurando o flowPane
+        flowPane.setHgap(30);
+        flowPane.setVgap(30);
+
+        flowPane.setMinWidth(1280);
+
+        flowPane.setStyle("-fx-background-color: #282828;");
+
+        return flowPane;
+    }
+
+    public static FlowPane criarTabelaOrdemAtual(List<OrdemServico> listaItens, int width, int height) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Map<String, String> dados;
+        FlowPane flowPane = new FlowPane();
+
+        HBox hbox;
+        for (OrdemServico item : listaItens){
+            dados = getDados(item);
+
+            hbox = criarComponenteNovoOrdemAtual(dados, width, height);
 
             flowPane.getChildren().add(hbox);
         }
@@ -103,14 +186,28 @@ public class OrdensCard extends ComponenteJavaFx {
             dados.put("Descrição", item.getDescricao());
         else
             dados.put("Descrição", "Indefinido");
-        dados.put("Tecnico", "Indefinido");
+
+        if (item.getTecnico() != null)
+            dados.put("Tecnico", item.getTecnico().getNome());
+        else
+            dados.put("Tecnico", "Indefinido");
 
         if (item.getMetodoPagamento() != null)
             dados.put("Metodo de Pagamento", item.getMetodoPagamento());
         else
             dados.put("Metodo de Pagamento", "Indefinido");
 
-        dados.put("Status", "Em andamento - ainda tá bugado");
+        if (item.isCancelado())
+            dados.put("Status", "Cancelado");
+
+        else if (item.isFinalizado())
+            dados.put("Status", "Finalizado");
+
+        else if (item.isEmAberto())
+            dados.put("Status", "Em Aberto");
+
+        else if (item.isEmAndamento())
+            dados.put("Status", "Em Andamento");
         return dados;
     }
 }
