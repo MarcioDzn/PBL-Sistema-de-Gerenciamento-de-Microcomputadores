7aa1229b938e1245b59944ff3376d6a981c5b2e4
Márcio Roberto <marcio.roberto0739@gmail.com>
2023-06-13 18:21:54 -0300

    :sparkles: feat: Classe LoginInfo

---
+++ PBL/src/main/java/com/example/pbl/model/LoginInfo.java
@@ -0,0 +1,58 @@
+package com.example.pbl.model;
+
+import java.io.Serializable;
+
+public class LoginInfo implements Serializable{
+    private int id;
+
+    private int idUsuario;
+    private String usuario;
+    private String senha;
+    private String tipo;
+
+    public LoginInfo(String usuario, String senha, String tipo) {
+        this.usuario = usuario;
+        this.senha = senha;
+        this.tipo = tipo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+}
