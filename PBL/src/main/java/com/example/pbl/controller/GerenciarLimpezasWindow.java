6a8c9c5bb1a377180a140d3c65feaa37b526e521
Márcio Roberto <marcio.roberto0739@gmail.com>
2023-06-14 16:46:02 -0300

    :sparkles: feat:(GerenciarLimpezasWindow): Implementação da tabela

--- PBL/src/main/java/com/example/pbl/controller/GerenciarLimpezasWindow.java
+++ PBL/src/main/java/com/example/pbl/controller/GerenciarLimpezasWindow.java
@@ -1,16 +1,26 @@
 package com.example.pbl.controller;
 
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import com.example.pbl.dao.DAO;
+import com.example.pbl.exceptions.ObjetoNaoEncontradoException;
 import com.example.pbl.model.Limpeza;
+import com.example.pbl.model.Montagem;
 import com.example.pbl.model.OutroComponente;
+import com.example.pbl.utils.componentesJavafx.ServicoCard;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class GerenciarLimpezasWindow {
 
@@ -43,6 +53,10 @@ public class GerenciarLimpezasWindow {
     @FXML
     private TextField txtPreco;
 
+    private FlowPane flowPaneLimpezas;
+    private List<Limpeza> listaLimpezas;
+    private List<Button> listaBotoes;
+
     @FXML
     void cadastrarAction(ActionEvent event) {
         Limpeza novaLimpeza = this.criarLimpeza();
@@ -50,6 +64,9 @@ public class GerenciarLimpezasWindow {
 
         if (limpezaValida){
             DAO.getLimpeza().criar(novaLimpeza);
+            this.listaLimpezas.add(novaLimpeza);
+
+            this.carregarScrollPaneServico();
             this.limparCampos();
             this.carregarCSS();
         }
@@ -77,7 +94,84 @@ public class GerenciarLimpezasWindow {
         assert txtDescricao != null : "fx:id=\"txtDescricao\" was not injected: check your FXML file 'GerenciarLimpezasWindow.fxml'.";
         assert txtPreco != null : "fx:id=\"txtPreco\" was not injected: check your FXML file 'GerenciarLimpezasWindow.fxml'.";
 
+        this.listaLimpezas = new LinkedList<>();
+        this.listaLimpezas = DAO.getLimpeza().buscarTodos();
+        this.listaBotoes = new LinkedList<>();
+
+        this.carregarScrollPaneServico();
         this.carregarCSS();
+    }
+
+    private void carregarScrollPaneServico(){
+        try {
+            try {
+                List<String> listaNomes = new LinkedList<>();
+                listaNomes.add("Descricao");
+                this.flowPaneLimpezas = ServicoCard.criarTabelaMenor(this.listaLimpezas, listaNomes, 405, 0);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+
+            this.scLimpezas.setContent(this.flowPaneLimpezas);
+
+            this.scLimpezas.setPannable(true);
+
+            this.scLimpezas.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+            this.scLimpezas.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+
+            this.selecionarBotoesServicos();
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    void selecionarBotoesServicos(){
+        HBox hboxMontagem;
+
+        this.listaBotoes.clear();
+
+        for (Node montagem : this.flowPaneLimpezas.getChildren()){
+            Button botao;
+            hboxMontagem = (HBox) montagem;
+
+            for (Node node : hboxMontagem.getChildren()){
+                if (node instanceof VBox){
+                    for (Node mostInnerNode : ((VBox) node).getChildren()){
+                        if (mostInnerNode instanceof VBox){
+                            for (Node innerNode: ((VBox) mostInnerNode).getChildren()){
+                                if (innerNode instanceof Button){
+                                    botao = (Button) innerNode;
+                                    this.listaBotoes.add(botao);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        this.removerServico();
+    }
+
+    void removerServico(){
+        for (Button botao : this.listaBotoes){
+            botao.setOnAction(e -> {
+                int index = this.listaBotoes.indexOf(botao);
+
+
+                try {
+                    DAO.getLimpeza().remover(this.listaLimpezas.get(index));
+                    this.listaBotoes.remove(index);
+                    this.listaLimpezas.remove(index);
+
+                    this.carregarScrollPaneServico();
+
+                } catch (ObjetoNaoEncontradoException ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
+        }
     }
 
     void limparCampos(){
@@ -90,6 +184,7 @@ public class GerenciarLimpezasWindow {
         this.txtCusto.clear();
         this.txtCusto.setPromptText("");
     }
+
     private boolean validarFormulario(Limpeza novaLimpeza){
         this.carregarCSS(); // Carrega o css padrão
         boolean limpezaValida = true;
@@ -120,6 +215,8 @@ public class GerenciarLimpezasWindow {
         txtPreco.setStyle("-fx-text-inner-color: white; -fx-background-color: #282828; -fx-border-color: white; -fx-border-width: 0 0 1.5 0");
         txtCusto.setStyle("-fx-text-inner-color: white; -fx-background-color: #282828; -fx-border-color: white; -fx-border-width: 0 0 1.5 0");
     }
+
+
 
     private Limpeza criarLimpeza(){
         String descricao = this.txtDescricao.getText();
